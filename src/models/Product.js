bf14e05b2f705e4cee342fad9fa3bd2c7ccdd378
Osmanity <mr.ibrahim@hotmail.se>
2025-04-01 21:23:46 +0200

    Uppdatera product modellen

--- src/models/Product.js
+++ src/models/Product.js
@@ -19,6 +19,10 @@ const productSchema = new mongoose.Schema({
     type: Number,
     default: 0,
     min: 0
+  },
+  category: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Category'
   }
 }, {
   timestamps: true
