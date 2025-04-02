0816dec2bda89cef781d844ce7a35541649d3377
Angie C <a.cajusol98@gmail.com>
2025-04-02 20:01:04 +0200

    Fix import product

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Category from "../models/Category.js"; 
 import { adminAuth } from "../middleware/auth.js";
+import Product from "../models/Product.js";
 
 const router = express.Router();
 
