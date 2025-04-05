7bd59bd2d7d9a843eaa3503ce0f52294c0a8f02e
Angie C <a.cajusol98@gmail.com>
2025-04-05 23:50:55 +0200

    route to get specific order

--- src/routes/orders.js
+++ src/routes/orders.js
@@ -1,5 +1,7 @@
 import express from "express";
 import Order from "../models/Order.js"; 
+import {auth, adminAuth } from "../middleware/auth.js";
+
 
 const router = express.Router();
 
@@ -15,6 +17,24 @@ router.get('/orders', adminAuth, async (req, res) => {
     } catch (error) {
         console.error('Error fetching orders:', error);
         res.status(500).json({ error: 'Failed to fetch orders: Something went wrong on the server.' });
+    }
+});
+
+//Get a specific order by ID (for user that made the purchase and admins)
+router.get('/orders/:id', auth, async (req, res) => {
+    const { id } = req.params;
+    try {
+    const order = await Order.findById(id).populate('products.product');
+    if (!order) {
+        return res.status(404).json({ error: `Order not found: The order with ID ${id} does not exist.` });
+    }
+    if (order.user.toString() !== req.user._id.toString() && !req.user.isAdmin) {
+        return res.status(403).json({ error: 'Access denied: You are not authorized to view this order.' });
+    }    
+    res.json(order);
+    } catch (error) {
+    console.error('Error fetching order:', error);
+    res.status(500).json({ error: 'Failed to fetch order: Something went wrong on the server.' });
     }
 });
 
